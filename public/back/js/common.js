e5e98c05f16b47181e662862f93199eb432d1d42
zxmingming <1026357729@qq.com>
2019-02-24 10:24:17 +0800

    公共功能部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -9,8 +9,8 @@
 
 
 // 1. 公共进度条功能
-// 在发送第一个ajax请求, 开启进度条
-// 在全部的ajax回来, 关闭进度条
+    // 在发送第一个ajax请求, 开启进度条
+    // 在全部的ajax回来, 关闭进度条
 
 // ajax全局事件
 // .ajaxComplete(fn);  每个ajax完成时, 都会调用fn回调函数   (完成: 不管成功还是失败)
@@ -36,3 +36,58 @@ $(document).ajaxStop(function() {
         NProgress.done();
     }, 500);
 });
+
+
+// 2. 公共的功能:
+    // (1) 左侧二级菜单的切换
+    // (2) 左侧整体菜单的切换
+    // (3) 公共的退出功能
+
+// 等待 dom 结构加载完成后, 才会执行
+$(function() {
+
+    // 1. 左侧二级菜单的切换
+    $('.lt_aside .category').click(function() {
+        // 找下一个兄弟元素, 切换显示
+        $(this).next().stop().slideToggle();
+    });
+
+
+    // 2. 左侧整体菜单的切换
+    $('.lt_topbar .icon_menu').click(function() {
+
+        // 让左侧整个菜单切换显示, 改左侧菜单的 left值
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+
+    });
+
+
+    // 3. 退出功能
+    // 点击菜单的退出按钮, 显示一个模态框, 询问用户
+    $('.lt_topbar .icon_logout').click(function() {
+        // 让模态框显示, modal('show)
+        $('#logoutModal').modal('show');
+    });
+
+    // 点击模态框的退出按钮, 表示确认退出
+    // 发送 ajax 请求, 让服务器端销毁用户的登录状态
+    $('#logoutBtn').click(function() {
+
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            dataType: 'json',
+            success: function(info) {
+                // console.log(info);
+                if (info.success) {
+                    // 退出成功, 跳转登录页面
+                    location.href = 'login.html';
+                }
+            }
+        })
+
+    })
+
+})
