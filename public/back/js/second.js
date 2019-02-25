786cef092f74bf9ad1ecc9df571d326251a2a402
zxmingming <1026357729@qq.com>
2019-02-25 22:23:14 +0800

    二级分类页完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -67,6 +67,7 @@ $(function() {
     });
 
 
+
     // 4. 给下拉菜单添加可选功能
     // 事件委托
     $('.dropdown-menu').on('click', 'a', function() {
@@ -75,6 +76,123 @@ $(function() {
         // console.log(txt);
         // 设置给 button 按钮
         $('#dropdownText').text(txt);
+
+        // 获取 id, 设置给隐藏域
+        var id = $(this).data('id');
+        // console.log(id);
+        
+        // 设置给隐藏域
+        $('[name="categoryId"]').val( id );
+
+        // 只要给隐藏域赋值了, 此时校验状态应该更新成功
+        $('#form').data('bootstrapValidator').updateStatus('categoryId', 'VALID');
+        
+    });
+
+
+
+    // 5. 完成文件上传初始化 => 使用jquery fileupload插件(实现文件上传)
+    $('#fileupload').fileupload({
+        dataType: 'json',
+        // 文件上传完成时的回调函数
+        done: function( e, data) {
+            // console.log(data);
+            var result = data.result; // 后台返回的结果
+            var picUrl = result.picAddr; // 获取返回的图片路径
+            // 设置给 img src
+            $('#imgBox img').attr('src', picUrl);
+
+            // 把路径赋值给 隐藏域
+            $('[name="brandLogo"]').val(picUrl);
+
+            // 只要隐藏域有值了, 就是更新成成功状态
+            $('#form').data('bootstrapValidator').updateStatus('brandLogo','VALID');
+        }
+    });
+
+
+    // 6. 直接进行表单校验
+    $('#form').bootstrapValidator({
+
+        // 配置 excluded 排除项, 对隐藏域完成校验
+        excluded: [],
+
+        // 配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        // 配置校验字段列表
+        fields: {
+            // 选择一级分类
+            categoryId: {
+                // 校验规则
+                validators: {
+                    notEmpty: {
+                        message: '请选择一级分类'
+                    }
+                }
+            },
+            // 请输入二级分类
+            brandName: {
+                // 校验规则
+                validators: {
+                    // 不能为空
+                    notEmpty: {
+                        // 提示信息
+                        message: '请输入二级分类名称'
+                    }
+                }
+            },
+            // 二级分类图片
+            brandLogo: {
+                // 校验规则
+                validators: {
+                    // 不能为空
+                    notEmpty: {
+                        // 提示信息
+                        message: '请选择图片'
+                    }
+                }
+            }
+        }
+
+    });
+
+
+    // 7. 注册表单校验成功事件, 阻止默认的提交, 通过ajaz提交
+    $('#form').on('success.form.bv', function( e ) {
+        // 阻止默认的提交
+        e.preventDefault();
+
+        // 发送ajax
+        $.ajax({
+            type: 'post',
+            url: '/category/addSecondCategory',
+            // 表单序列化, 自动将所有配置了 name 属性的 input 值进行拼接, 用于提交
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function( info ) {
+                // console.log(info);
+                if (info.success) {
+                    // 添加成功
+                    // 关闭模态框
+                    $('#addModal').modal('hide');
+                    // 页面重新渲染, 第一页
+                    currentPage = 1;
+                    render();
+
+                    // 将表单元素重置 (内容和状态都重置)
+                    $('#form').data('bootstrapValidator').resetForm(true);
+
+                    // button 和 img 不是表单元素, 手动设置
+                    $('#dropdownText').text('请选择一级分类');
+                    $('#imgBox img').attr('src', './images/none.png');
+                }
+            }
+        })
         
     })
 
--- public/back2/js/second.js
+++ public/back2/js/second.js
@@ -71,5 +71,109 @@ $(function() {
     var txt = $(this).text();
     // 设置给 button 按钮
     $('#dropdownText').text(txt);
+
+    // 获取 id , 设置给 隐藏域
+    var id = $(this).data('id');
+    // 设置给隐藏域
+    $('[name="categoryId"]').val( id );
+
+    // 只要给隐藏域赋值了, 此时校验状态应该更新成成功
+    $('#form').data('bootstrapValidator').updateStatus('categoryId', 'VALID');
+  });
+
+
+  // 4. 完成文件上传初始化
+  $('#fileupload').fileupload({
+    dataType: 'json',
+    // 文件上传完成时的回调函数
+    done: function( e, data ) {
+      console.log( data );
+      var result = data.result; // 后台返回的结果
+      var picUrl = result.picAddr; // 获取返回的图片路径
+      // 设置给 img src
+      $('#imgBox img').attr('src', picUrl);
+
+      // 把路径赋值给 隐藏域
+      $('[name="brandLogo"]').val(picUrl);
+
+      // 只要隐藏域有值了, 就是更新成成功状态
+      $('#form').data('bootstrapValidator').updateStatus('brandLogo', 'VALID');
+    }
+  });
+
+
+  // 5. 直接进行校验
+  $('#form').bootstrapValidator({
+    // 配置 excluded 排除项, 对隐藏域完成校验
+    excluded: [],
+
+    // 配置图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
+    // 配置校验字段列表
+    fields: {
+      // 选择一级分类
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: '请选择一级分类'
+          }
+        }
+      },
+      // 输入二级分类名称
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: '请输入二级分类名称'
+          }
+        }
+      },
+      // 二级分类图片
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: '请选择图片'
+          }
+        }
+      }
+    }
+  });
+
+
+  // 6. 注册表单校验成功事件, 阻止默认的提交, 通过ajax提交
+  $('#form').on('success.form.bv', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: 'post',
+      url: '/category/addSecondCategory',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function( info ) {
+        console.log( info );
+        if (info.success) {
+          // 添加成功
+          // 关闭模态框
+          $('#addModal').modal('hide');
+          // 页面重新渲染, 第一页
+          currentPage = 1;
+          render();
+
+          // 将表单元素重置 (内容和状态都重置)
+          $('#form').data('bootstrapValidator').resetForm(true);
+
+          // button 和 img 不是表单元素, 手动重置
+          $('#dropdownText').text('请选择一级分类');
+          $('#imgBox img').attr('src', './images/none.png');
+        }
+      }
+    })
+
   })
+
+
 })
