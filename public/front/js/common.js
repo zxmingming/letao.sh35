6867ec43c0b156c8fd8ebf1729ba984f0ead7964
zxmingming <1026357729@qq.com>
2019-02-27 22:22:58 +0800

    首页完成

---
+++ public/front/js/common.js
@@ -0,0 +1,13 @@
+// 区域滚动
+mui('.mui-scroll-wrapper').scroll({
+    deceleration: 0.0005, // 阻尼系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    indicators: false, // 是否显示滚动条
+});
+
+
+// 轮播图自动轮播
+//获得slider插件对象
+var gallery = mui('.mui-slider');
+gallery.slider({
+  interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+});
